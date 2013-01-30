213a0310f0ab4fbec17bbe3cd4d99bb3e578bc2d
Nasir Jamal <nas35_in@yahoo.com>
2013-01-30 14:50:24 +0000

    Add pascal triangle and f iterative exercises

--- chapter1_1_2.scm
+++ chapter1_1_2.scm
@@ -6,6 +6,10 @@
 (provide Ackermann)
 (provide fib-recursive)
 (provide fib-iterative)
+(provide count-change)
+(provide f-recursive)
+(provide f-iterative)
+(provide pascal-triangle-element)
 
 ; example
 (define (factorial-recursive n)
@@ -68,3 +72,55 @@
   (iter 1 0 n)
 )
                                 
+; example Counting change
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kind-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kind-of-coins 0)) 0)
+        (else (+ (cc amount (- kind-of-coins 1))
+                 (cc (- amount (first-denomination kind-of-coins))
+                     kind-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)
+        ))
+
+
+; exercise 1.11 
+; f(n) = n if n < 3
+; f(n) = f(n-1) + 2f(n-2) + 3f(n-3)
+(define (f-recursive n) ; this function is not part of the solution
+  (if (< n 3)
+      n
+      (+ (f-recursive (- n 1)) (* 2 (f-recursive (- n 2))) (* 3 (f-recursive (- n 3))))
+      )
+)
+
+(define (f-iterative n) 
+  (define (iteration a b c count)
+    (if (< count 3)
+        a
+        (iteration (+ a (* 2 b) (* 3 c)) a b (- count 1))
+         )
+   )
+  (if (< n 3)
+      n
+      (iteration 2 1 0 n)
+      )
+  )
+
+; exercise 1.12 Pascal's triangle, give column and row number to return the value
+; recursive process
+(define (pascal-triangle-element column row)
+  (if (or (= row 1) (= column 1) (= row column))
+      1
+      (+ (pascal-triangle-element (- column 1) (- row 1)) (pascal-triangle-element column (- row 1)))
+  )
+)
